a327ca5b6f07215a3f4e811750e2b5b8410c5b3e
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2023-06-09 09:00:26 +0530

    Create Assignment_7.c

---
+++ Assignment_7.c
@@ -0,0 +1,43 @@
+
+#include <xc.h>
+#include <stdlib.h>
+#include<pic18f4550.h>
+
+#define Buzzer LATAbits.LATA5
+
+unsigned int count=0;
+
+void TMR1ISR(){
+    if(TMR1IF==1){
+        TMR1L=0x20;
+        TMR1H=0xD1;
+        count++;
+    
+    if(count>=1000){
+        Buzzer=~Buzzer;
+        count =0;
+    }
+    TMR1IF=0;
+    }
+}
+
+void main(void) {
+    
+    TRISB=0x00;
+    TRISAbits.TRISA5=0x00;
+    
+    GIE=1;
+    PEIE=1;
+    TMR1IE=1;
+    TMR1IF=0;
+    
+    T1CON =0x80;
+    
+    TMR1L=0x20;
+    TMR1H=0xD1;
+    
+    TMR1ON=1;
+    
+    while(1);
+    return;
+}
